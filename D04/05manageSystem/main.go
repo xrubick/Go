1c6d222625ffa4a64d3e3cdb91fede3d0bdc6123
xrubick <xrubick@163.com>
2020-03-26 22:13:02 +0800

    function system

--- D04/05manageSystem/main.go
+++ D04/05manageSystem/main.go
@@ -8,6 +8,51 @@ import (
 //import "fmt"
 //import "os"
 
+//结构体
+type stu struct {
+	id   int
+	name string
+}
+
+//构造函数
+func newStu(id int, name string) *stu {
+	return &stu{
+		id:   id,
+		name: name,
+	}
+}
+
+var all = make(map[int]*stu, 20)
+
+func listAll() {
+	for k, v := range all {
+		fmt.Printf("id:%d name:%s\n", k, v.name)
+	}
+}
+
+func addSomeone() {
+	var (
+		id   int
+		name string
+	)
+	fmt.Println("id:")
+	fmt.Scanln(&id)
+	fmt.Println("name:")
+	fmt.Scanln(&name)
+	newStu := newStu(id, name)
+	all[id] = newStu
+
+}
+
+func delSomeone() {
+	var delID int
+	fmt.Println("delId:")
+	fmt.Scanln(&delID)
+
+	delete(all, delID)
+
+}
+
 func main() {
 	for {
 		//系统菜单
@@ -27,10 +72,13 @@ func main() {
 		switch num {
 		case 1:
 			fmt.Println("1.查询全体人员")
+			listAll()
 		case 2:
 			fmt.Println("2.增加成员")
+			addSomeone()
 		case 3:
 			fmt.Println("3.删除成员")
+			delSomeone()
 		case 4:
 			os.Exit(1)
 
