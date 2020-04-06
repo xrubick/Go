0efc7dc401db5b332e516fa3d1b63ced2cf40059
xrubick <xrubick@163.com>
2020-04-06 21:54:42 +0800

    http post

--- D08锁和协议/07http/client/main.go
+++ D08锁和协议/07http/client/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
+	"strings"
 )
 
 // //Client发送http请求
@@ -25,31 +25,54 @@ import (
 // 	fmt.Println(string(body))
 // }
 
-//带参数的get请求
-func main() {
-	apiurl := "http://127.0.0.1:9000/get"
-	data := url.Values{}
-	data.Set("name", "二哈")
-	data.Set("age", "2")
-	u, err := url.ParseRequestURI(apiurl)
-	if err != nil {
-		fmt.Println("parse uri failed:", err)
-	}
+// //带参数的get请求
+// func main() {
+// 	//get请求
+// 	apiurl := "http://127.0.0.1:9000/get"
+// 	data := url.Values{}
+// 	data.Set("name", "二哈")
+// 	data.Set("age", "2")
+// 	u, err := url.ParseRequestURI(apiurl)
+// 	if err != nil {
+// 		fmt.Println("parse uri failed:", err)
+// 	}
 
-	//url encode
-	u.RawQuery = data.Encode()
-	fmt.Println(u.String())
-	resp, err := http.Get(u.String())
+// 	//url encode
+// 	u.RawQuery = data.Encode()
+// 	fmt.Println(u.String())
+// 	resp, err := http.Get(u.String())
+// 	if err != nil {
+// 		fmt.Println("get failed:", err)
+// 		return
+// 	}
+// 	b, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("get resp failed:", err)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+// 	fmt.Println(string(b))
+// }
+
+//post请求
+func main() {
+	url := "http://127.0.0.1:9000/post"
+	// // 表单数据
+	// contentType := "application/x-www-form-urlencoded"
+	// data := "name=二哈&age=2"
+	// json
+	contentType := "application/json"
+	data := `{"name":"二哈","age":2}`
+	resp, err := http.Post(url, contentType, strings.NewReader(data))
 	if err != nil {
-		fmt.Println("get failed:", err)
+		fmt.Println("post failed:", err)
 		return
 	}
+	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("get resp failed:", err)
 		return
 	}
-	defer resp.Body.Close()
 	fmt.Println(string(b))
-
 }
--- D08锁和协议/07http/server/main.go
+++ D08锁和协议/07http/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+//handle 带参数的get请求
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	data := r.URL.Query()
@@ -14,14 +16,32 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(answer))
 }
 
-func main() {
+//handle post请求
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	// //请求类型为application/x-www-form-urlencoded时解析form数据
+	// r.ParseForm()
+	// fmt.Println(r.PostForm) // 打印form数据
+	// fmt.Println(r.PostForm.Get("name"), r.PostForm.Get("age"))
+	//请求类型为application/json时从r.Body读取数据
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("read request failed:", err)
+		return
+	}
+	fmt.Println(string(b))
+	answer := `{"status": "ok"}`
+	w.Write([]byte(answer))
+}
 
+func main() {
 	//有参数get请求server端
-	http.HandleFunc("/", getHandler)
+	//http.HandleFunc("/get", getHandler)
+	http.HandleFunc("/post", postHandler)
+
 	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
 		fmt.Println("http server failed:", err)
 		return
 	}
-
 }
